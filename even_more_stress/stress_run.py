4e686efb3930aa87dfd018cbe0db4d59121fcc99
MT <mt@gm.com>
2024-09-23 17:14:11 +0200

    first commit, working solution, focus on TODOs, tests failing

---
+++ even_more_stress/stress_run.py
@@ -0,0 +1,118 @@
+from time import time
+import logging
+from subprocess import run, PIPE, call, check_output
+import re
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+def map_regexp_first_hit_to_numeric(regexp, output, mapping):
+    # TODO: fail on no hits
+    search = re.search(regexp, output)
+    try:
+        start = search.group(1)
+        end = search.group(2)
+    except AttributeError as err:
+        logger.error(f"Error {err} while parsing output \n{output}.")
+        raise err
+    val = f"{start}.{end}"
+    return mapping(val)
+
+
+class CassandraStressRun:
+    def __init__(self, thread_id, duration, node_ip):
+        self.thread_id = thread_id
+        self.duration = duration
+        self.node_ip = node_ip
+        self.op_rate = 0
+        self.latency_mean = 0
+        self.latency_99th = 0
+        self.latency_max = 0
+        self.start_time = 0
+        self.end_time = 0
+        # TODO: pin specific digest
+        self._image_name = "scylladb/cassandra-stress"
+        self._cassandra_threads = 10
+        # self._run_command = f'docker run {self._image_name} "cassandra-stress write duration={self.duration}s -rate threads={self._cassandra_threads} -node {self.node_ip}"'
+        self._run_command = f"docker run {self._image_name} 'cassandra-stress write duration={self.duration}s -rate threads={self._cassandra_threads} -node {self.node_ip}'"
+        # self._run_command = ["docker", "run", self._image_name, f'cassandra-stress write duration={self.duration}s -rate threads={self._cassandra_threads} -node {self.node_ip}']
+
+    @property
+    def time_taken(self):
+        return self.end_time - self.start_time
+
+    @property
+    def run_command(self):
+        # cmd = [
+        #     'docker', 'run', '-it', 'scylladb/cassandra-stress',
+        #     'cassandra-stress', 'write', 'duration=10s',
+        #     '-rate', 'threads=10', '-node', '127.0.0.1'
+        # ]
+        # cmd = [el.splitlines() for el in self._run_command]
+        cmd = self._run_command
+        logger.debug(f"Running cmd = {cmd}")
+        return cmd
+
+    def run_stress_test(self):
+        self.start_time = time()
+        logging.info(
+            f"Thread {self.thread_id}: Starting stress test with duration={self.duration} on node {self.node_ip}"
+        )
+        # result = run(self.run_command, stdout=PIPE, stderr=PIPE, check=True)
+        # print(check_output(self.run_command))
+        result = run(self.run_command, stdout=PIPE, stderr=PIPE, shell=True)
+        if result.returncode != 0:
+            logger.error(f"Command failed with return code {result.returncode}")
+            logger.error("Error output (stderr):")
+            logger.error(result.stderr)
+            logger.error(result.stdout)
+            raise Exception(f"StdError {result.stderr}, {result.stdout}!")
+        self.end_time = time()
+        self.parse_results(result.stdout.decode("utf-8"))
+        logging.info(
+            f"Thread {self.thread_id}: Completed stress test. Duration: {self.time_taken:.2f} seconds"
+        )
+        return self
+
+    @staticmethod
+    def _match_op_rate(output):
+        return map_regexp_first_hit_to_numeric(
+            regexp=r"Op rate\s*:\s*(\d+),(\d+)", output=output, mapping=float
+        )
+
+    @staticmethod
+    def _match_latency_mean(output):
+        return map_regexp_first_hit_to_numeric(
+            regexp=r"Latency mean\s*:\s*(\d+).(\d+)", output=output, mapping=float
+        )
+
+    @staticmethod
+    def _match_latency_99th(output):
+        return map_regexp_first_hit_to_numeric(
+            regexp=r"Latency 99th percentile\s*:\s*(\d+).(\d+)",
+            output=output,
+            mapping=float,
+        )
+
+    @staticmethod
+    def _match_latency_max(output):
+        return map_regexp_first_hit_to_numeric(
+            regexp=r"Latency max\s*:\s*(\d+).(\d+)", output=output, mapping=float
+        )
+
+    def parse_results(self, output):
+        self.op_rate = self._match_op_rate(output)
+        self.latency_mean = self._match_latency_mean(output)
+        self.latency_99th = self._match_latency_99th(output)
+        self.latency_max = self._match_latency_max(output)
+        logging.debug(
+            f"Thread {self.thread_id}: Parsed Results - Op rate: {self.op_rate}, Latency mean: {self.latency_mean}, "
+            f"Latency 99th: {self.latency_99th}, Latency max: {self.latency_max}"
+        )
+
+    def __repr__(self):
+        return f"Run {self.thread_id=} - {self.op_rate=}, {self.latency_mean=}, {self.latency_99th=}, {self.latency_max=}"
---
+++ tests/test_main.py
@@ -0,0 +1,45 @@
+from even_more_stress.main import run_stress_tests, present_results
+from even_more_stress.stress_run import CassandraStressRun
+
+
+def run_fake_process(n):
+    return n
+
+
+class TestingConcurentRunner:
+
+    def test_it_is_posibile_to_run_5_concurent_processes(self):
+        return_5_results = run_stress_tests(
+            n_runs=5, node_ip="ip-stub", run_function=run_fake_process
+        )
+        assert len(return_5_results) == 5
+
+    def test_it_is_posibile_to_run_0_concurent_processes(self):
+        return_0_results = run_stress_tests(
+            n_runs=0, node_ip="ip-stub", run_function=run_fake_process
+        )
+        assert len(return_0_results) == 0
+
+
+class TestMainUtils:
+
+    def test_presenting_end_results(self):
+        run_1 = CassandraStressRun(thread_id=1, duration=2, node_ip="111.1.1.1")
+        run_1.op_rate = 10
+        run_1.latency_mean = 20
+        run_1.latency_99th = 30
+        run_1.latency_max = 40
+
+        run_2 = CassandraStressRun(thread_id=2, duration=10, node_ip="111.1.1.1")
+        run_2.op_rate = 5
+        run_2.latency_mean = 6
+        run_2.latency_99th = 7
+        run_2.latency_max = 8
+
+        total_op_rate, avg_latency_mean, avg_latency_99th, latency_max_stdev = (
+            present_results([run_1, run_2])
+        )
+        assert total_op_rate == 15
+        assert avg_latency_mean == 26 / 2
+        assert avg_latency_99th == 37 / 2
+        assert latency_max_stdev == 22.627416997969522
